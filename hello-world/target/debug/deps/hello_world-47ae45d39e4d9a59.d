9e08198d479eae505fd86df0833457f48c37130e
Mapleryy <idunnotho77@gmail.com>
2025-09-06 11:09:43 -0400

    TEst

---
+++ hello-world/target/debug/deps/hello_world-47ae45d39e4d9a59.d
@@ -0,0 +1,5 @@
+D:\Github\hello-world\hello-world\target\debug\deps\hello_world-47ae45d39e4d9a59.d: src\main.rs
+
+D:\Github\hello-world\hello-world\target\debug\deps\libhello_world-47ae45d39e4d9a59.rmeta: src\main.rs
+
+src\main.rs:
